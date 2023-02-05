661b694f268bba9b766d42ade29ba09a2c2c168d
kwstas1kosfi <kwstas.kosfi@gmail.com>
2023-02-05 15:48:16 +0200

    mob bills

---
+++ ex2bills/Mobile_Bills.c
@@ -0,0 +1,160 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main()
+{
+	int seconds,sms,mb;
+	float cost;
+	printf("Monnthly seconds : \n");
+	scanf("%d",&seconds);
+	printf("Amount of sms : \n");
+	scanf("%d",&sms);
+	printf("Amount of MB : \n");
+	scanf("%d",&mb);
+	if ((seconds<=60)&&(sms<=30)&&(mb<=200))
+	{
+		cost=5.5;
+	}
+	else if ((seconds>60)||(sms>30)||(mb>200))
+	{
+		if (seconds<=100)
+		{
+			if (sms>30)
+			{
+				if (mb>200)
+				{ 
+					cost=5.5+(seconds-60)*0.01 + sms*0.02 + mb*0.015;
+				}
+				else
+				{
+					cost=5.5 + (seconds-60)*0.01 + sms*0.02;
+				}
+			}
+			else 
+			{
+				if (mb>200)
+				{
+					cost=5.5 + (seconds-60)*0.01 + mb*0.015;
+				}
+				else
+				{
+					cost=5.5 + (seconds-60)*0.01;
+				}	
+			}
+		}
+		else if (seconds<=200)
+		{
+			if (sms>30)
+			{
+				if (mb>200)
+				{
+					cost=5.5 + (seconds-60)*0.01 + (seconds-60-101)*0.03 + sms*0.02 + mb*0.015;
+				}
+				else
+				{
+					cost=5.5 + (seconds-60)*0.01 + (seconds-60-101)*0.03 + sms*0.02;
+				}
+			}
+			else 
+			{
+				if (mb>200)
+				{
+					cost=5.5 + (seconds-60)*0.01 + (seconds-60-101)*0.03 + mb*0.015;
+				}
+				else
+				{
+					cost=5.5 + (seconds-60)*0.01 + (seconds-60-101)*0.03;
+				}
+			}
+		}
+		else
+		{
+			if (sms>30)
+			{
+				if (mb>200)
+				{
+					cost=5.5 + (seconds-60)*0.01 + (seconds-60-101)*0.03 + (seconds-60-101-201)*0.05 + sms*0.02 + mb*0.015;
+				}
+				else
+				{
+					cost=5.5 + (seconds-60)*0.01 + (seconds-60-101)*0.03 + (seconds-60-101-201)*0.05 + sms*0.02;
+				}
+			}
+			else
+			{
+				if(mb>200)
+				{
+					cost=5.5 + (seconds-60)*0.01 + (seconds-60-101)*0.03 + (seconds-60-101-201)*0.05 + mb*0.015;
+				}
+				else 
+				{
+					cost=5.5 + (seconds-60)*0.01 + (seconds-60-101)*0.03 + (seconds-60-101-201)*0.05;
+				}
+			}
+		}
+	}
+	char impo;
+	printf("If he is an important customer type y if he is not press n\n");
+	do{
+		scanf("%c",&impo);
+		
+	}
+	while ((impo!='y')&&(impo!='n'));
+	if (impo=='y')
+	{
+		printf("He will have 5 precent discount\n");
+		//cost=cost*(0.95);
+	}
+	printf("When the bill did publich?\n");
+	/*int mday,mon,year;
+	do{
+		scanf("%d",&year);
+		if((year!=2023)&&(year!=2022))
+		{
+			printf("wrong year\n");
+		}
+	}while((year!=2022)&&(year!=2023));
+	do{
+		scanf("%d",&mon);
+		if((mon<1)||(mon>12))
+		{
+			printf("wrong month\n");
+		}
+	}while((mon<1)||(mon>12));
+	do{
+		scanf("%d",&mday);
+		if((mday<1)||(mon>30))
+		{
+			printf("wrong day\n");
+		}
+	}while((mday<1)||(mday>30));
+	printf("The bill did publish on %d/%d/%d\n",mday,mon,year);*/
+	//Fuction which shows us the current time
+	time_t curtime;
+	time(&curtime);
+	printf("Published  %s\n", ctime(&curtime));
+	printf("You want to pay now? Press y if you dont press n\n");
+	char pay;
+	do
+	{
+		scanf("%c",&pay);
+	} while ((pay!='y')&&(pay!='n'));
+	if((pay=='y')&&(impo=='y'))
+	{
+		cost=cost*(0.90);
+	}
+	else if((pay=='n')&&(impo=='y'))
+	{
+		cost=cost*(0.95);
+	}
+	else if ((pay=='y')&&(impo=='n'))
+	{
+		cost=cost*(0.97);
+	}	
+	
+	printf("The cost is %0.2f Euros",cost);
+	
+	return 0;
+}
---
+++ ex5/ex5reversed.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int a[5],b[5];
+    int i=0;
+    for (i=0;i<5;i++)
+    {
+        printf("a[%d]=",i+1);
+        scanf("%d",&a[i]);
+    }
+    for (i=0;i<5;i++)
+    {
+        b[5-i]=a[i];
+    }
+    for(i=0;i<5;i++)
+    {
+        printf("a[%d]=%d\t",i+1,a[i]);
+        //printf("b[%d]=%d\t",i+1,b[i+1]);
+    }
+    printf("\nReversed array\n");
+    for(i=0;i<5;i++)
+    {
+        //printf("a[%d]=%d\t",i+1,a[i]);
+        printf("b[%d]=%d\t",i+1,b[i+1]);
+    }
+    for(i=0;i<5;i++)
+    {
+        if (a[i]<0)
+        {
+            a[i]=a[i]*(-1);
+        }
+    }
+    printf("\nPositive Array\n");
+    for(i=0;i<5;i++)
+    {
+        printf("a[%d]=%d\t",i+1,a[i]);
+        //printf("b[%d]=%d\t",i+1,b[i+1]);
+    }
+    int sum=0,p=0;
+    printf("\n");
+    int j;
+    for (i=0;i<5;i++)
+    {
+        p=0;
+        for(j=2;j<a[i];j++)
+        {
+            if(a[i]%j==0)
+            {
+                p=1;
+                //sum=sum+1;
+                //printf("a[%d] = %d is prime\n",i+1,a[i]);                
+                break;
+            }
+        }
+        if(p==0)
+        {
+            printf("\t %d is prime",a[i]);
+            sum=sum+1;
+
+        }
+    }
+    printf("\nsum=%d",sum);
+    return 0;
+}
